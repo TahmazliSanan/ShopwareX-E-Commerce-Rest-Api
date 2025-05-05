655d7ace3dc227d044326e85dc33681031fe7310
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-05 17:10:28 +0400

    Create AppDbContext

---
+++ ShopwareX/DataContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopwareX.Entities;
+
+namespace ShopwareX.DataContext
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
