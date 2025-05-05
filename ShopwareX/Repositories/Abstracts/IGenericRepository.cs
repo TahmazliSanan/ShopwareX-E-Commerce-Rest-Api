d120a9355106e55338ca4713c38071e0706e8118
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-05 19:58:38 +0400

    Create IGenericRepository and its implementation

---
+++ ShopwareX/Repositories/Abstracts/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using ShopwareX.Entities;
+
+namespace ShopwareX.Repositories.Abstracts
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task AddAsync(TEntity entity);
+        Task<TEntity?> GetByIdAsync(long id);
+        IQueryable<TEntity> GetAll();
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(long id);
+        Task SaveAsync();
+    }
+}
