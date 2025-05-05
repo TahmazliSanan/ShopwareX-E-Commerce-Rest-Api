d0d9a5e7ad5a4a86f28fe5175030b973aea3ee8c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-05 20:53:47 +0400

    Create IGenderService and its implementation

---
+++ ShopwareX/Services/Abstracts/IGenderService.cs
@@ -0,0 +1,13 @@
+﻿using ShopwareX.Entities;
+
+namespace ShopwareX.Services.Abstracts
+{
+    public interface IGenderService
+    {
+        Task<Gender> AddAsync(Gender gender);
+        Task<Gender?> GetByIdAsync(long id);
+        IQueryable<Gender> GetAll();
+        Task<Gender?> UpdateAsync(long id, Gender gender);
+        Task<Gender?> DeleteAsync(long id);
+    }
+}
---
+++ ShopwareX/Services/Concretes/GenderService.cs
@@ -0,0 +1,45 @@
+﻿using ShopwareX.Entities;
+using ShopwareX.Repositories.Abstracts;
+using ShopwareX.Services.Abstracts;
+
+namespace ShopwareX.Services.Concretes
+{
+    public class GenderService(IGenderRepository genderRepository) : IGenderService
+    {
+        private readonly IGenderRepository _genderRepository = genderRepository;
+
+        public async Task<Gender> AddAsync(Gender gender)
+        {
+            await _genderRepository.AddAsync(gender);
+            await _genderRepository.SaveAsync();
+            return gender;
+        }
+
+        public async Task<Gender?> DeleteAsync(long id)
+        {
+            var existGender = await _genderRepository.GetByIdAsync(id);
+            await _genderRepository.DeleteAsync(id);
+            return existGender;
+        }
+
+        public IQueryable<Gender> GetAll() => _genderRepository.GetAll();
+
+        public async Task<Gender?> GetByIdAsync(long id) => await _genderRepository.GetByIdAsync(id);
+
+        public async Task<Gender?> UpdateAsync(long id, Gender gender)
+        {
+            var existGender = await _genderRepository
+                .GetByIdAsync(id)
+                ?? throw new Exception("Gender not found");
+            
+            var newGender = new Gender
+            {
+                Name = existGender.Name
+            };
+
+            await _genderRepository.UpdateAsync(newGender);
+            await _genderRepository.SaveAsync();
+            return existGender;
+        }
+    }
+}
