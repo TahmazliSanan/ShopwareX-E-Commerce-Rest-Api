9b098a5559a222e23eb99eeef68253cbb4c17ef8
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-05 21:26:59 +0400

    Create MappingProfile

---
+++ ShopwareX/Mapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ShopwareX.Dtos.Gender;
+using ShopwareX.Entities;
+
+namespace ShopwareX.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<GenderCreateDto, Gender>();
+            CreateMap<Gender, GenderResponseDto>();
+        }
+    }
+}
