e9380108e5de181fc9e5f103c9799cd9ce14ed8e
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-08 00:20:55 +0400

    Create UserDtos

---
+++ ShopwareX/Dtos/User/UserResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopwareX.Dtos.User
+{
+    public class UserResponseDto
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateOnly? DateOfBirth { get; set; }
+        public long GenderId { get; set; }
+        public long RoleId { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
