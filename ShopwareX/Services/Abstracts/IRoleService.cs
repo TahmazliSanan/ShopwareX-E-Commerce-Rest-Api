84b5cc5adaa0765e11d303964626622aace5044f
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-07 18:21:30 +0400

    Create IRoleService and its implementation

---
+++ ShopwareX/Services/Abstracts/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using ShopwareX.Entities;
+
+namespace ShopwareX.Services.Abstracts
+{
+    public interface IRoleService
+    {
+        Task<Role> AddRoleAsync(Role role);
+        Task<Role?> GetRoleByIdAsync(long id);
+        Task<Role?> GetRoleWithUsersAsync(long id);
+        Task<IEnumerable<Role>> GetAllRolesAsync();
+        Task<Role?> UpdateRoleAsync(long id, Role role);
+        Task<Role?> DeleteRoleByIdAsync(long id);
+    }
+}
---
+++ ShopwareX/Services/Concretes/RoleService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopwareX.Entities;
+using ShopwareX.Repositories.Abstracts;
+using ShopwareX.Services.Abstracts;
+
+namespace ShopwareX.Services.Concretes
+{
+    public class RoleService(IRoleRepository roleRepository) : IRoleService
+    {
+        private readonly IRoleRepository _roleRepository = roleRepository;
+
+        public async Task<Role> AddRoleAsync(Role role)
+        {
+            await _roleRepository.AddAsync(role);
+            await _roleRepository.SaveAsync();
+            return role;
+        }
+
+        public async Task<Role?> DeleteRoleByIdAsync(long id)
+        {
+            var existRole = await GetRoleWithUsersAsync(id);
+
+            if (existRole is not null)
+            {
+                existRole.IsDeleted = true;
+                existRole.UpdatedAt = DateTime.UtcNow;
+
+                existRole.Users
+                    .ToList()
+                    .ForEach(u =>
+                    {
+                        u.IsDeleted = true;
+                        u.UpdatedAt = DateTime.Now;
+                    });
+
+                await _roleRepository.SaveAsync();
+            }
+
+            return existRole;
+        }
+
+        public async Task<IEnumerable<Role>> GetAllRolesAsync()
+            => await _roleRepository.GetAll().ToListAsync();
+
+        public async Task<Role?> GetRoleByIdAsync(long id)
+            => await _roleRepository.GetByIdAsync(id);
+
+        public async Task<Role?> GetRoleWithUsersAsync(long id)
+            => await _roleRepository.GetRoleWithUsersAsync(id);
+
+        public async Task<Role?> UpdateRoleAsync(long id, Role role)
+        {
+            var existRole = await GetRoleByIdAsync(id);
+
+            if (existRole is not null)
+            {
+                existRole.Name = role.Name;
+
+                await _roleRepository.UpdateAsync(existRole);
+                await _roleRepository.SaveAsync();
+            }
+
+            return existRole;
+        }
+    }
+}
