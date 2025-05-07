a051c8ec979de83fd500ef6a3088d17e6e83c5ac
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-07 18:21:08 +0400

    Create IRoleRepository and its implementation

---
+++ ShopwareX/Repositories/Abstracts/IRoleRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShopwareX.Entities;
+
+namespace ShopwareX.Repositories.Abstracts
+{
+    public interface IRoleRepository : IGenericRepository<Role>
+    {
+        Task<Role?> GetRoleWithUsersAsync(long id);
+    }
+}
---
+++ ShopwareX/Repositories/Concretes/RoleRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopwareX.DataContext;
+using ShopwareX.Entities;
+using ShopwareX.Repositories.Abstracts;
+
+namespace ShopwareX.Repositories.Concretes
+{
+    public class RoleRepository(AppDbContext context)
+        : GenericRepository<Role>(context), IRoleRepository
+    {
+        private readonly DbSet<Role> _roles = context.Set<Role>();
+
+        public async Task<Role?> GetRoleWithUsersAsync(long id)
+            => await _roles
+                .Include(g => g.Users)
+                .FirstOrDefaultAsync(g => g.Id == id && g.IsDeleted == false);
+    }
+}
