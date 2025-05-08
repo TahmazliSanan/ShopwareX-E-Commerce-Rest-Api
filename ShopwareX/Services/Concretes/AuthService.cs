2ec067fb331e14c69b07f94addf23c7ce7ec3c25
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-09 00:59:47 +0400

    Create IAuthService and its implementation

---
+++ ShopwareX/Services/Concretes/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using ShopwareX.Dtos.Login;
+using ShopwareX.Repositories.Abstracts;
+using ShopwareX.Services.Abstracts;
+
+namespace ShopwareX.Services.Concretes
+{
+    public class AuthService(IUserRepository userRepository, IJwtGenerator jwtGenerator) : IAuthService
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly IJwtGenerator _jwtGenerator = jwtGenerator;
+
+        public async Task<LoginResponseDto?> LoginUserAsync(LoginRequestDto dto)
+        {
+            var existUserByEmail = await _userRepository.GetUserByEmailAsync(dto.Email);
+
+            if (existUserByEmail is not null)
+            {
+                bool isMatched = BCrypt.Net.BCrypt
+                    .Verify(dto.Password, existUserByEmail.HashedPassword);
+
+                if (isMatched)
+                {
+                    var jwt = _jwtGenerator.GenerateJwt(existUserByEmail);
+
+                    return new LoginResponseDto
+                    {
+                        Jwt = jwt,
+                        Email = existUserByEmail.Email,
+                        RoleId = existUserByEmail.Role?.Id ?? 0
+                    };
+                }
+            }
+
+            return null;
+        }
+    }
+}
