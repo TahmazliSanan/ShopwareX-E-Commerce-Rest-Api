27d4527d5fff4250ce0118e353edcd2f17b8b2b3
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-08 12:07:33 +0400

    Create IUserRepository and its implementation

---
+++ ShopwareX/Repositories/Concretes/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopwareX.DataContext;
+using ShopwareX.Entities;
+using ShopwareX.Repositories.Abstracts;
+
+namespace ShopwareX.Repositories.Concretes
+{
+    public class UserRepository(AppDbContext context)
+        : GenericRepository<User>(context), IUserRepository
+    {
+        private readonly DbSet<User> _users = context.Set<User>();
+
+        public async Task<User?> GetUserByEmail(string email)
+            => await _users.FirstOrDefaultAsync(u => u.Email.Trim().ToLower()
+            .Equals(email.Trim().ToLower()) && u.IsDeleted == false);
+    }
+}
