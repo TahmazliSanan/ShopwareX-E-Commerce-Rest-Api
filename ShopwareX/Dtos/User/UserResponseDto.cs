e0910346e28abc271e78caa409a0cc0917efeab3
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-11 00:08:56 +0400

    Add new properties: GenderName and RoleName

--- ShopwareX/Dtos/User/UserResponseDto.cs
+++ ShopwareX/Dtos/User/UserResponseDto.cs
@@ -7,7 +7,9 @@
         public string Email { get; set; } = null!;
         public DateOnly? DateOfBirth { get; set; }
         public long GenderId { get; set; }
+        public string GenderName { get; set; } = null!;
         public long RoleId { get; set; }
+        public string RoleName { get; set; } = null!;
         public bool IsDeleted { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
