4a5aed92720288cf56098ad8b99d13451175523d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-10 23:40:38 +0400

    Modified

--- ShopwareX/DataContext/AppDbContext.cs
+++ ShopwareX/DataContext/AppDbContext.cs
@@ -8,6 +8,8 @@ namespace ShopwareX.DataContext
         public DbSet<Gender> Genders { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
             => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
