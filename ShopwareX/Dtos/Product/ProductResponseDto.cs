ce1086dbf5feac1d8cc6f0281087eb187aa30689
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-15 00:12:07 +0400

    Create ProductDtos

---
+++ ShopwareX/Dtos/Product/ProductResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopwareX.Dtos.Product
+{
+    public class ProductResponseDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public long CategoryId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
