8f200ab86310050ca7c7181250b3e099dfa22cbe
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-17 20:27:29 +0400

    Create entity: Order

---
+++ ShopwareX/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopwareX.Entities
+{
+    public class Order : BaseEntity
+    {
+        public decimal TotalPrice { get; set; }
+        public long UserId { get; set; }
+        public User User { get; set; } = null!;
+        public ICollection<OrderItem> Items { get; set; } = [];
+    }
+}
