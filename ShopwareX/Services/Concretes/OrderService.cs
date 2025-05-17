60da17be7c3f1147583cc0c536f229a7394cd1dc
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-17 23:16:18 +0400

    Create IOrderService and its implementation

---
+++ ShopwareX/Services/Concretes/OrderService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ShopwareX.Dtos.Order;
+using ShopwareX.Entities;
+using ShopwareX.Repositories.Abstracts;
+using ShopwareX.Services.Abstracts;
+
+namespace ShopwareX.Services.Concretes
+{
+    public class OrderService(IMapper mapper, IOrderRepository orderRepository) : IOrderService
+    {
+        private readonly IMapper _mapper = mapper;
+        private readonly IOrderRepository _orderRepository = orderRepository;
+
+        public async Task<OrderResponseDto> AddOrderAsync(OrderCreateDto dto)
+        {
+            var order = _mapper.Map<Order>(dto);
+            await _orderRepository.AddAsync(order);
+            await _orderRepository.SaveAsync();
+            return _mapper.Map<OrderResponseDto>(order);
+        }
+
+        public async Task<OrderResponseDto> DeleteOrderByIdAsync(long id)
+        {
+            var existOrder = await _orderRepository.GetOrderByIdAsync(id);
+
+            if (existOrder is not null)
+            {
+                existOrder.IsDeleted = true;
+                existOrder.UpdatedAt = DateTime.Now;
+
+                existOrder.Items
+                    .ToList()
+                    .ForEach(i =>
+                    {
+                        i.IsDeleted = true;
+                        i.UpdatedAt = DateTime.Now;
+                    });
+
+                await _orderRepository.SaveAsync();
+            }
+
+            return _mapper.Map<OrderResponseDto>(existOrder);
+        }
+
+        public async Task<IEnumerable<OrderResponseDto>> GetAllOrdersAsync()
+        {
+            var orders = await _orderRepository.GetAllOrders().ToListAsync();
+            return _mapper.Map<IEnumerable<OrderResponseDto>>(orders);
+        }
+
+        public async Task<OrderResponseDto> GetOrderByIdAsync(long id)
+        {
+            var existOrder = await _orderRepository.GetOrderByIdAsync(id);
+            return _mapper.Map<OrderResponseDto>(existOrder);
+        }
+    }
+}
