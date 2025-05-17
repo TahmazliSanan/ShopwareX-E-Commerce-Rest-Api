9320c67fdda68059bdd90a2c407a80569c9f6fbc
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-17 22:14:41 +0400

    Create OrderItemDtos

---
+++ ShopwareX/Dtos/OrderItem/OrderItemResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopwareX.Dtos.OrderItem
+{
+    public class OrderItemResponseDto
+    {
+        public long Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public long ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+    }
+}
