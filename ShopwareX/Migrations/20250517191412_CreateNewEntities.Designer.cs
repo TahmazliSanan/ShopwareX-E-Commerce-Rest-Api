9a275c1547b217e1c50d977df50c99934375e63d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-17 23:15:12 +0400

    Create new migrations

--- ShopwareX/Migrations/20250517191412_CreateNewEntities.Designer.cs
+++ ShopwareX/Migrations/20250517191412_CreateNewEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopwareX.DataContext;
 
@@ -11,9 +12,11 @@ using ShopwareX.DataContext;
 namespace ShopwareX.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250517191412_CreateNewEntities")]
+    partial class CreateNewEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
