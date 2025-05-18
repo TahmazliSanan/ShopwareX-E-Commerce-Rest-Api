584a44ce796c4e45c0d703e9d29bf2ce08cf38c3
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-05-18 12:58:54 +0400

    Modify method

--- ShopwareX/Services/Abstracts/IOrderService.cs
+++ ShopwareX/Services/Abstracts/IOrderService.cs
@@ -4,7 +4,7 @@ namespace ShopwareX.Services.Abstracts
 {
     public interface IOrderService
     {
-        Task<OrderResponseDto> AddOrderAsync(OrderCreateDto dto);
+        Task<OrderResponseDto?> AddOrderAsync(OrderCreateDto dto, long userId);
         Task<OrderResponseDto> GetOrderByIdAsync(long id);
         Task<IEnumerable<OrderResponseDto>> GetAllOrdersAsync();
         Task<OrderResponseDto> DeleteOrderByIdAsync(long id);
--- ShopwareX/Services/Concretes/OrderService.cs
+++ ShopwareX/Services/Concretes/OrderService.cs
@@ -7,17 +7,48 @@ using ShopwareX.Services.Abstracts;
 
 namespace ShopwareX.Services.Concretes
 {
-    public class OrderService(IMapper mapper, IOrderRepository orderRepository) : IOrderService
+    public class OrderService(IMapper mapper, IOrderRepository orderRepository,
+        IProductRepository productRepository) : IOrderService
     {
         private readonly IMapper _mapper = mapper;
         private readonly IOrderRepository _orderRepository = orderRepository;
+        private readonly IProductRepository _productRepository = productRepository;
 
-        public async Task<OrderResponseDto> AddOrderAsync(OrderCreateDto dto)
+        public async Task<OrderResponseDto?> AddOrderAsync(OrderCreateDto dto, long userId)
         {
-            var order = _mapper.Map<Order>(dto);
-            order.TotalPrice = order.Items.Sum(i => i.Quantity * i.Product.Price);
+            var productIds = dto.Items
+                .Select(i => i.ProductId)
+                .ToList();
+
+            var products = _productRepository.GetProductsByIds(productIds).ToList();
+
+            if (products.Count != productIds.Count)
+            {
+                return null;
+            }
+
+            var items = dto.Items.Select(i =>
+            {
+                var product = products.First(p => p.Id == i.ProductId);
+                return new OrderItem
+                {
+                    ProductId = product.Id,
+                    Quantity = i.Quantity,
+                    UnitPrice = product.Price,
+                    CreatedAt = DateTime.Now
+                };
+            }).ToList();
+
+            var order = new Order
+            {
+                UserId = userId,
+                Items = items,
+                TotalPrice = items.Sum(i => i.UnitPrice * i.Quantity)
+            };
+
             await _orderRepository.AddAsync(order);
             await _orderRepository.SaveAsync();
+
             return _mapper.Map<OrderResponseDto>(order);
         }
 
